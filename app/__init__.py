0af2bc5c97a8e023c8dace4ebe7d207943716db5
Talasta <m.choulika@icloud.com>
2019-10-22 11:38:32 +0200

    update

--- app/__init__.py
+++ app/__init__.py
@@ -25,17 +25,20 @@ def create_app(config_name):
 	@app.route('/sir', methods=['POST'])
 	def ranti_bot():
 		command_text = request.data.get('text')
-		command_text = command_text.split(' ')
-		slack_uid = request.data.get('user_id')
-		slackhelper = SlackHelper()
-		slack_user_info = slackhelper.user_info(slack_uid)
-		actions = Actions(slackhelper, slack_user_info)
+		if command_text != None:
+			command_text = command_text.split(' ')
+			slack_uid = request.data.get('user_id')
+			slackhelper = SlackHelper()
+			slack_user_info = slackhelper.user_info(slack_uid)
+			actions = Actions(slackhelper, slack_user_info)
 
-		if command_text[0] not in allowed_commands:
-			response_body = {'text': 'Invalid Command Sent - `/ranti help` for available commands'}
+			if command_text[0] not in allowed_commands:
+				response_body = {'text': 'Invalid Command Sent - `/sir help` for available commands'}
 
-		if command_text[0] == 'help':
-			response_body = actions.help()
+			if command_text[0] == 'help':
+				response_body = actions.help()
+		else:
+			response_body = {'text': 'Invalid Command Sent - `/sir help` for available commands'}
 
 		response = jsonify(response_body)
 		response.status_code = 200
