24dce4ec2d1fda183d66e371af27c52c438f9aa1
Talasta <m.choulika@icloud.com>
2019-10-22 14:53:16 +0200

    fix error 500

--- app/utils/gappshelper.py
+++ app/utils/gappshelper.py
@@ -15,50 +15,23 @@ class GappsHelper:
 
 	def credentials_from_env(self, scopes):
 		keyfile_dict = {}
+		keyfile_dict["private_key"] = get_env('SC_PRIVATE_KEY')
+		keyfile_dict["auth_uri"] = get_env('SC_AUTH_URI')
+		keyfile_dict["token_uri"] = get_env('SC_TOKEN_URI')
 		try:
-			keyfile_dict['private_key'] = get_env('SC_PRIVATE_KEY')
-		except Exception:
-			raise Exception('spam', 'SC_PRIVATE_KEY')
-		try:
-			keyfile_dict["auth_uri"] = get_env('SC_AUTH_URI')
-		except Exception:
-			raise Exception('spam', 'SC_AUTH_URI')
-		try:
-			keyfile_dict["token_uri"] = get_env('SC_TOKEN_URI')
-		except Exception:
-			raise Exception('spam', 'SC_TOKEN_URI')
-		try:
-			keyfile_dict["client_email"] = get_env('SC_CLIENT_EMAIL')
-		except Exception:
-			raise Exception('spam', 'SC_CLIENT_EMAIL')
-		try:
-			keyfile_dict["private_key_id"] = get_env('SC_PRIVATE_KEY_ID')
-		except Exception:
-			raise Exception('spam', 'SC_PRIVATE_KEY_ID')
-		try:
-			keyfile_dict["client_id"] = get_env('SC_CLIENT_ID')
-		except Exception:
-			raise Exception('spam', 'SC_CLIENT_ID')
-		try:
-			signer = crypt.Signer.from_string(keyfile_dict['private_key'])
-		except Exception:
-			raise Exception('spam', 'Signer')
-		try:
-			credential = ServiceAccountCredentials(
-				keyfile_dict["client_email"],
-				signer,
-				scopes=scopes,
-				private_key_id=keyfile_dict["private_key_id"],
-				client_id=keyfile_dict["client_id"],
-				token_uri=keyfile_dict.get('token_uri', GOOGLE_TOKEN_URI),
-				revoke_uri=keyfile_dict.get('revoke_uri', GOOGLE_REVOKE_URI),
-			)
-		except Exception:
-			raise Exception('spam', 'ServiceAccountCredentials')
-		try:
-			credential._private_key_pkcs8_pem = keyfile_dict['private_key']
-		except Exception:
-			raise Exception('spam', 'private_key')
+			signer = crypt.Signer.from_string(keyfile_dict["private_key"])
+		except Exception as e:
+			raise('', keyfile_dict.get("private_key"))
+		credential = ServiceAccountCredentials(
+			get_env('SC_CLIENT_EMAIL'),
+			signer,
+			scopes=scopes,
+			private_key_id=get_env('SC_PRIVATE_KEY_ID'),
+			client_id=get_env('SC_CLIENT_ID'),
+			token_uri=keyfile_dict.get('token_uri', GOOGLE_TOKEN_URI),
+            revoke_uri=keyfile_dict.get('revoke_uri', GOOGLE_REVOKE_URI),
+		)
+		credential._private_key_pkcs8_pem = keyfile_dict['private_key']
 		return credential
 
 	def open_sheet(self):
