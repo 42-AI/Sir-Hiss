2b1b75a58447aade01c7d21577a50d864b11be7c
Talasta <m.choulika@icloud.com>
2019-10-22 18:33:41 +0200

    working on register and unregister

--- worker.py
+++ worker.py
@@ -4,8 +4,17 @@ from app.utils.slackhelper import SlackHelper
 
 # Main function
 def main():
+   user_info = {
+      'user': {
+         'name': 'maxime', 
+         'id': 'a0123456789',
+         },
+      }
    slackhelper = SlackHelper()
-   actions = Actions(slackhelper)
+   actions = Actions(slackhelper, user_info=user_info)
+   print(actions.register())
+   print(actions.unregister())
+   print(actions.unregister())
    # actions.notify_channel()
 
 if __name__ == '__main__':
