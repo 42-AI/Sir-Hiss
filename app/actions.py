6f71563f43771ba06740982a8a90e22f86e165ac
Talasta <m.choulika@icloud.com>
2019-10-22 18:33:41 +0200

    working on register and unregister

--- app/actions.py
+++ app/actions.py
@@ -4,13 +4,13 @@ from datetime import datetime, date, timedelta
 from config import get_env
 
 HELPER_MSG = """Available commands:
--> `/bootcamp_python register`
--> `/bootcamp_python unregister`
--> `/bootcamp_python subject day[xx]`
--> `/bootcamp_python correction day[xx]`
--> `/bootcamp_python students`
--> `/bootcamp_python info`
--> `/bootcamp_python help`
+	`/bootcamp_python register`
+	`/bootcamp_python unregister`
+	`/bootcamp_python subject day[xx]`
+	`/bootcamp_python correction day[xx]`
+	`/bootcamp_python students`
+	`/bootcamp_python info`
+	`/bootcamp_python help`
 """
 
 class Actions:
@@ -29,22 +29,61 @@ class Actions:
 				self.user_id = tmp.get('id')
 		########################
 
+	""" Check if action has logged user_info """
+	def mandatoryUserInfo(f):
+		def wrapper(self, *args, **kwargs):
+			if self.user_info is None:
+				return "Error, no user logged"
+			return f(self, *args, **kwargs)
+		return wrapper
+
+	""" Check if the user is registered at the bootcamp """
+	def mandatoryRegistered(f):
+		def wrapper(self, *args, **kwargs):
+			for index, row in enumerate(self.sheet.get_all_records()):
+				if row['user_id'] == self.user_id:
+					break
+			else:
+				return "Error, user not registered at the bootcamp python"
+			return f(self, *args, **kwargs)
+		return wrapper
+
 	def help(self):
 		text_detail = HELPER_MSG
 		return text_detail
 
+	""" Register a user if he is not yet registered to the bootcamp """
+	@mandatoryUserInfo
 	def register(self):
-		text_detail = "register"
-		return text_detail
+		# Check if the user is already registered
+		for row in self.sheet.get_all_records():
+			if row['user_id'] == self.user_id:
+				return "Already registered"
+		# Update with user info
+		self.sheet.insert_row([self.user_name, self.user_id], 2)
+		return "You are now registered to the bootcamp python"
 
+	""" Unregister a user if he is registered to the bootcamp """
+	@mandatoryUserInfo
+	@mandatoryRegistered
 	def unregister(self):
-		text_detail = "unregister"
-		return text_detail
+		for index, row in enumerate(self.sheet.get_all_records()):
+			if row['user_id'] == self.user_id:
+				break
+		else:
+			return "You are not registered"
+		index += 2
+		self.sheet.delete_row(index)
+		return "You have been unregistered form the bootcamp python"
 
+	@mandatoryUserInfo
+	@mandatoryRegistered
 	def subject(self, *args):
-		text_detail = "subject" + str(self.sheet.get_all_records())
+		text_detail = "subject"
 		return text_detail
 
+	@mandatoryUserInfo
+	@mandatoryRegistered
 	def info(self):
 		text_detail = "info about : {} with id:[{}]".format(
 				self.user_name,
